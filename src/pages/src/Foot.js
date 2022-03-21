7694f4320ffe2fc0053defb82683d99ad7491236
Yi Zhao <11612917@mail.sustech.edu.cn>
2022-03-21 15:44:28 -0500

    [joey]add project github link

--- src/pages/src/Foot.js
+++ src/pages/src/Foot.js
@@ -6,6 +6,7 @@ const Foot = () => {
 		<>
             <div className="copyright">
 				<p>License MIT Copyright © 2022 <a href={"https://www.yizhao.tech/about?yi"}>Yi Zhao</a> contributor(s)</p>
+				<a href="https://github.com/yizhao1998/DFA-Visualization">Github</a>
             </div>
 		</>
 	);
